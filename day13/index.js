0e002d7b73356d3f64f10a7ab7a03cf7565d5cfb
hasunggeong <kiu995405@gmail.com>
2025-11-17 10:23:12 +0900

    ...

--- day13/index.js
+++ day13/index.js
@@ -1,3 +1,29 @@
 export default async function main() {
-    console.log('hello js')
-}
+    console.log('start app')
+
+    let currentIndex = 0;
+    const menuitems = document.querySelectorAll(".menu-item");
+
+    console.log(menuitems)
+
+    menuitems[currentIndex].classList.remove('selcet')
+
+    menuitems[currentIndex].classList.add("select")
+
+    window.addEventListener("keydown", (e) => {
+        console.log(e.key)
+
+        if(e.key == "ArrowUp") {
+            currentIndex--
+            if(currentIndex < 0) {
+                currentIndex = 2
+            }
+        }
+        else if(e.key == "ArrowDown") {
+            currentIndex++
+            currentIndex %= 3
+        }
+        console.log(currentIndex)
+        menuitems[currentIndex].classList.add('select')
+    })
+    }
